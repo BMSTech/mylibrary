341981b226700a226145cc9330808d8b1aaa0f33
Tiến Hiệu <duongnguyen1216@gmail.com>
2018-04-01 12:49:26 +0700

    Update string :strawberry:

--- models/author.py
+++ models/author.py
@@ -7,7 +7,7 @@ class Author(models.Model):
     _name = 'mylib.author'
     _description = 'My library authors'
 
-    name = fields.Char("Tên tác giả")
-    image = fields.Binary("Ảnh tác giả")
+    name = fields.Char("Tên")
+    image = fields.Binary("Ảnh")
     nation = fields.Many2one('res.country', 'Quốc gia')
     description = fields.Text("Tiểu sử")
